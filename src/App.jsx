6eb6bbae3d882fa30938d206305c5d7059adfed9
Francisco Silberberg <fransilber16@gmail.com>
2024-09-20 10:50:10 -0300

    test buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Register from './components/Register'
 import Login from './components/Login'
@@ -17,6 +17,8 @@ function App() {
             <h1>Title</h1>
         </header>
 
+        <Link to={'/Login'} >login</Link>
+        <Link to={'/Project'} >Projects</Link>
         <Routes>
             <Route path='/Register' element={<Register />} />
             <Route path='/Login' element={<Login />} />
