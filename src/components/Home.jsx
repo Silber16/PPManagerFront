d7c951e2b1707d45779467e6abbd3eb2dadbc8e8
Francisco Silberberg <fransilber16@gmail.com>
2025-10-14 21:47:40 -0300

    adding maintenance msg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,52 +10,66 @@ import AccessMessage from "./AccessMessage"
 export default function Home() {
 
     const [isLoggedIn, setIsLoggedIn] = useState(false)
-    
+
     useEffect(() => {
-        axios.get(`${import.meta.env.VITE_BACK_URI}/Account/CheckAuth`, {withCredentials:true})
+        axios.get(`${import.meta.env.VITE_BACK_URI}/Account/CheckAuth`, { withCredentials: true })
             .then(res => {
                 setIsLoggedIn(res.data.isAuthenticated)
             })
             .catch(() => {
                 setIsLoggedIn(false)
-                
+
             })
     }, [])
-    
-  return (
-    <>
-       <section className="home-sec">
-       {isLoggedIn ? 
-       (
-            <>
-                <header className="home-sec__header">
-                    <h1 className="home-sec__header--title"><a href="#">ProjectPilot</a></h1>
-                    <Link to={'/UserData'} className="home-sec__header--user" ><span className="material-symbols-outlined">account_circle</span></Link>
-                </header>
-                <Projects />
-            </>
-       ) 
-       : 
-       (
-            <>
-                <AccessMessage /> 
-                <header className="home-sec__header">
-                    <h1 className="home-sec__header--title"><a href="#">ProjectPilot</a></h1>
-                </header>
-                <div className="home-sec__landing">
-                    <p className="home-sec__landing--phrase">
-                        Manage your projects <br /> in the simplest way. 
-                        <br /><span style={{color: 'var(--o-color)', fontWeight: '600'}}>100% free.</span>
-                    </p>
-                    <Link to={'/Register'} className="home-sec__landing--registerBtn">Register/Login</Link>
-                    <Register className="home-sec__landing--registerComp" />
-                </div>
-                <span style={{position: 'absolute', bottom: '1rem', fontSize: '2.5rem', color: 'var(--b-color)'}} className="material-symbols-outlined">keyboard_arrow_down</span>
-            </>
-       )
-       }
-       </section>
-       {isLoggedIn == false && <AppDesc />}
-    </>
-  )
+
+    return (
+        <>
+            <section className="home-sec">
+                {import.meta.env.UNDER_MAINTENANCE == 'Y'
+                    ? (
+                        <>
+                            <header className="home-sec__header">
+                                <h1 className="home-sec__header--title"><a href="#">ProjectPilot</a></h1>
+                            </header>
+                            <div className="home-sec__landing">
+                                <p className="home-sec__landing--phrase">
+                                    The app is under <span style={{ color: 'var(--o-color)', fontWeight: '600' }}>maintenance</span>.
+                                </p>
+                            </div>
+                        </>
+                    )
+                    : (
+                        isLoggedIn
+                            ? (
+                                <>
+                                    <header className="home-sec__header">
+                                        <h1 className="home-sec__header--title"><a href="#">ProjectPilot</a></h1>
+                                        <Link to={'/UserData'} className="home-sec__header--user" ><span className="material-symbols-outlined">account_circle</span></Link>
+                                    </header>
+                                    <Projects />
+                                </>
+                            )
+                            : (
+                                <>
+                                    <AccessMessage />
+                                    <header className="home-sec__header">
+                                        <h1 className="home-sec__header--title"><a href="#">ProjectPilot</a></h1>
+                                    </header>
+                                    <div className="home-sec__landing">
+                                        <p className="home-sec__landing--phrase">
+                                            Manage your projects <br /> in the simplest way.
+                                            <br /><span style={{ color: 'var(--o-color)', fontWeight: '600' }}>100% free.</span>
+                                        </p>
+                                        <Link to={'/Register'} className="home-sec__landing--registerBtn">Register/Login</Link>
+                                        <Register className="home-sec__landing--registerComp" />
+                                    </div>
+                                    <span style={{ position: 'absolute', bottom: '1rem', fontSize: '2.5rem', color: 'var(--b-color)' }} className="material-symbols-outlined">keyboard_arrow_down</span>
+                                </>
+                            )
+                    )
+                }
+            </section>
+            {isLoggedIn == false && <AppDesc />}
+        </>
+    )
 }
